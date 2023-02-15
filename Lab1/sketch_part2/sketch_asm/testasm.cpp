d5352434e387b417148b0fcd444e1809fb76841e
James Zhao <j.d.zhao@yale.edu>
2023-02-14 20:45:19 -0500

    Reworked testasm to use assembly

--- Lab1/sketch_part2/sketch_asm/testasm.cpp
+++ Lab1/sketch_part2/sketch_asm/testasm.cpp
@@ -119,10 +119,6 @@ __attribute__((used))
 unsigned int testasm(unsigned char a, unsigned char b) {
   unsigned int out;
 
-  unsigned int sum = sumval(a, b);
-  unsigned int diff = diffval(a, b);
-  out = sum + diff;
-
   asm volatile (R"(
 
    rjmp start_of_assembly_testasm
@@ -134,17 +130,45 @@ unsigned int testasm(unsigned char a, unsigned char b) {
 
 start_of_assembly_testasm:
    ; Your assembly language program goes here
-   ;
-   ; For this example, your program must not modify any registers other than 
-   ; r24 and r25. In other words, save and restore all the registers that
-   ; are modified by your code.
 
-   ; Tell the compiler to move the argument out into registers 
+   ; Tell the compiler to move the arguments a, b into registers 
    ; r24, r25 respectively
    ;
-   mov r24,%0
-
-   ;rjmp end_of_assembly_testasm
+   mov r24,%1
+   mov r25,%2
+   ;
+   ;  --- YOUR CODE GOES HERE ---
+   ;       r24 = a, r25 = b
+   ;
+   call sumval
+   mov r22, r24 ; move r24 -> r22
+   mov r23, r25 ; move r25 -> r23
+   mov r24,%1 ; re-copy a and b
+   mov r25,%2
+   
+   call diffval
+   add r24, r22
+   adc r25, r23
+   rjmp end_of_assembly_testasm
+   
+sumval:
+   add r24, r25
+   clr r25
+   adc r25, r25
+   ret
+diffval:
+   cp r25, r24
+   brcc no_carry
+   ; r24 >= r25
+   sub r24, r25
+   clr r25
+   ret
+no_carry:
+   ; r25 > r24
+   sub r25, r24
+   mov r24, r25
+   clr r25
+   ret
    ;
    ;  --- YOUR CODE ENDS ---
    ;      The result must be in the register pair r25:r24
@@ -154,7 +178,7 @@ end_of_assembly_testasm:
    ; -- move r25:r24 to the 16-bit word in variable out
    movw %0,r24
 
-)" : "=r" (out)  /* out is %0, and a 16-bit variable that is modified
+)" : "=w" (out)  /* out is %0, and a 16-bit variable that is modified
         by the assembly language */
    : "r" (a), "r" (b)  /* a is %1, b is %2, both are register-sized
         variables */
