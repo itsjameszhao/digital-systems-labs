f3caebdffcbff2948c7c38cc2ddbf13a20d2eb0c
vnotchris <chris06mtz@gmail.com>
2023-04-01 03:53:24 -0400

    final touchups: need review to make sure nothing broke

--- Lab3/sketch_part2/sketch_part2.ino
+++ Lab3/sketch_part2/sketch_part2.ino
@@ -1,9 +1,15 @@
+// based on provided example: https://www.instructables.com/Simple-Arduino-and-HC-SR04-Example/
+// color changes at MAX_DISTANCE/2
+// color value is static: either entirely green or red, determined by threshold mentioned above
+// brightness control imported from part 1
+
 const int photocellPin = A0;
 const int redPin = 9;
 const int greenPin = 11; 
 const int bluePin = 10;
 const int echoPin = 12;
 const int trigPin = 13;
+const int MAX_DISTANCE = 10;
 
 void setup() {
   Serial.begin(9600);
@@ -20,27 +26,26 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
+
+  // in the code below, map could return values outside of the range (0, 255) but 
+  // this does not cause unwanted behavior
+  // edit: added floor and ceiling
   distance = duration / 58.2;
-  int mappedDistance = map(distance, 0, 10, 0, 255);
-  Serial.print("Distance is: ");
-  Serial.println(distance);
+  if(distance > MAX_DISTANCE) {
+    distance = MAX_DISTANCE;
+  } else {
+    distance = max(0, distance);
+  }
+  int mappedDistance = map(distance, 0, MAX_DISTANCE, 0, 255);
   
   // Read from Photocell:
   int photocellValue = analogRead(photocellPin);
-  int mappedPhotocellValue = map(photocellValue, 200, 1023, 0, 255);
-  Serial.print("Mapped photocell value is: ");
-  Serial.println(mappedPhotocellValue);
-  Serial.print("Photocell value is: ");
-  Serial.println(photocellValue);
-  Serial.print("HIGh: ");
-  Serial.println(HIGH);
+  int mappedPhotocellValue = map(photocellValue, 0, 1023, 0, 255);
   
-  
-  analogWrite(greenPin, 0);
   if (mappedDistance < 128) {
-    analogWrite(greenPin, 0);
+    analogWrite(greenPin, mappedPhotocellValue);
     analogWrite(redPin, 0);
-    analogWrite(bluePin, mappedPhotocellValue);
+    analogWrite(bluePin, 0);
   }
   else {
     analogWrite(greenPin, 0);
@@ -48,6 +53,5 @@ void loop() {
     analogWrite(redPin, mappedPhotocellValue);
   }
   
-  Serial.println(photocellValue);
   delay(10);
 }
--- Lab3/sketch_part3/sketch_part3.ino
+++ Lab3/sketch_part3/sketch_part3.ino
@@ -5,23 +5,39 @@
 #define OLED_CLK 7
 #include <Adafruit_SSD1306.h>
 
+
+// ENCODER CODE GREATLY INSPIRED BY: https://forum.arduino.cc/t/trouble-reading-rotary-encoder/52571
+// OLED PONG CODE GREATLY INSPIRED BY: https://github.com/makeabilitylab/arduino/blob/master/OLED/BallBounce/BallBounce.ino
+
 const int encoder0PinA = 2;
 const int encoder0PinB = 3;
+
+// Used for velocity
+// values (128, 255] => positive velocity, larger is greater magnitude
+// values [0, 128] => negative velocity, smaller is greater magnitude
+// i.e. absolute difference between encoder0Pos and 128 determines magnitude
 const int MIN_VAL = 0;
 const int MAX_VAL = 255;
-static unsigned long last_interrupt_time = 0;
-static unsigned long ENCODER_DELAY_FILTER = 100;
 int encoder0Pos = 128;
 
+static unsigned long last_interrupt_time = 0;
+static unsigned long ENCODER_DELAY_FILTER = 100;
+
+// OLED setup
 Adafruit_SSD1306 disp(128,64,OLED_SI,OLED_CLK,OLED_DC,OLED_RST,OLED_CS);
+int WIDTH;
+int HEIGHT;
 
 // Ball variables
-const int _ballRadius = 4;
-int _xBall = 0;
-int _yBall = 0;
-int _xDir = 1;
-int _yDir = 1;
+const int ballRadius = 4;
+// Position of ball
+int xBall = 0;
+int yBall = 0;
+// Direction of ball
+int xDir = 1;
+int yDir = 1;
 const int DELAY_LOOP_MS = 5;
+int MAX_VELOCITY = 8;
 
 void setup() {
   // For rotary
@@ -33,37 +49,56 @@ void setup() {
   Serial.println ("start reading encoder");
   Serial.println (encoder0Pos, DEC);
 
-  // For LED
+  // For OLED
   disp.begin(SSD1306_SWITCHCAPVCC);
   disp.clearDisplay();
   disp.display();
+  WIDTH = disp.width()
+  HEIGHT = disp.height()
 
   // Initialize ball to the center of the screeen
-  _xBall = disp.width() / 2;
-  _yBall = disp.height() / 2;
+  xBall = WIDTH / 2;
+  yBall = HEIGHT / 2;
 
 }
 
 void loop() {
   disp.clearDisplay();
-  // Update ball based on speed location
-  int ballVelocity = (encoder0Pos - 128)`;
-
-  // Ball direction vars and speed
-  _xBall += _xDir * ballVelocity;
-  _yBall += _yDir * ballVelocity;
-
-  // Check for ball bounce
-  if(_xBall - _ballRadius <= 0 || _xBall + _ballRadius >= disp.width()){
-    _xDir = _xDir * -1; // reverse x direction
+  // Update velocity based on encoder0Pos
+  // Every turns to the encoder changes speed by one, capped at magnitude of MAX_VELOCITY
+  int ballVelocity = (encoder0Pos - 128);
+  // add a maximum speed
+  if(ballVelocity < 0){
+    ballVelocity = max(ballVelocity, MAX_VELOCITY * -1);
+  } else {
+    ballVelocity = min(ballVelocity, MAX_VELOCITY);
   }
-  
-  if(_yBall - _ballRadius <= 0 || _yBall + _ballRadius >= disp.height()){
-    _yDir = _yDir * -1; // reverse y direction
+
+  // little bonus if you made it all the way to 0 or 255: 50% speed boost for your efforts
+  if(encoder0Pos == 0 || encoder0Pos == 255) {
+    if(ballVelocity < 0){
+        ballVelocity -= (MAX_VELOCITY / 2)
+    } else {
+        ballVelocity += (MAX_VELOCITY / 2)
+    }
+  }
+
+  // Using velocity and directions, update position of ball
+  xBall += xDir * ballVelocity;
+  yBall += yDir * ballVelocity;
+
+  // Check for ball bounce: xBall and yBall represent the center coordinates of the ball
+  // check that center +/- radius does not come into contact with boundary, if it does, reverse directions (xDir & yDir),
+  // not encoder0Pos
+  if(xBall - ballRadius <= 0 || xBall + ballRadius >= WIDTH){
+    xDir = xDir * -1;
+  } 
+  if(yBall - ballRadius <= 0 || yBall + ballRadius >= HEIGHT){
+    yDir = yDir * -1;
   }
 
   // Draw circle
-  disp.drawCircle(_xBall, _yBall, _ballRadius, SSD1306_WHITE);
+  disp.drawCircle(xBall, yBall, ballRadius, SSD1306_WHITE);
   
   // Render buffer to screen
   disp.display();
@@ -73,16 +108,7 @@ void loop() {
   }
 }
 
-void tryIncrementEncoder() {
-  encoder0Pos = min(MAX_VAL, encoder0Pos + 1);
-  Serial.println(encoder0Pos);
-}
-
-void tryDecrementEncoder() {
-  encoder0Pos = max(MIN_VAL, encoder0Pos - 1);
-  Serial.println(encoder0Pos);
-}
-
+// using interrupts allows us to be responsive to rotations
 void encoderAInterrupt(){
   // Filter out spurious interrupts
   unsigned long interrupt_time = millis();
@@ -103,10 +129,12 @@ void encoderBInterrupt(){
 
 void doEncoderA(){ 
   // Simply increment the encoder when we interrupt
-  tryIncrementEncoder();
+  encoder0Pos = min(MAX_VAL, encoder0Pos + 1);
+  Serial.println(encoder0Pos);
 } 
 
 void doEncoderB(){
   // Simply decrement the encoder when we interrupt
-  tryDecrementEncoder();
+  encoder0Pos = max(MIN_VAL, encoder0Pos - 1);
+  Serial.println(encoder0Pos);
 }
